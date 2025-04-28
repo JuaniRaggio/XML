7e156698cc5f693589fe8055d0f9d4dd8bbcbba4
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-04-27 23:41:09 -0300

    Ejercicio 2

---
+++ xpath_ejercicios/tp4/Ejercicio2/ej2.sh
@@ -0,0 +1,12 @@
+# Ej 2.1
+xmllint --xpath "APML/Body/Profile//Concepts/Concept[contains(@*,'business')]" ../apml1.xml
+
+# Ej 2.2
+xmllint --xpath "APML/Body/Profile//Concepts/Concept[@value>=0.95]" ../apml1.xml
+
+# Ej 2.3
+xmllint --xpath "APML/Body/Profile/@name[count(..//Concepts/Concept) < (count(//APML/Body/Profile//Concepts/Concept) div count(//APML/Body/Profile))]" ../apml1.xml
+
+# Ej 2.4
+xmllint --xpath "APML/Body/Profile//Concepts/Concept/@key[starts-with(., 'a') or starts-with(., 'e') or starts-with(., 'i') or starts-with(., 'o') or starts-with(., 'u')]" ../apml1.xml
+
---
+++ xpath_ejercicios/tp4/ejercicio3.sh
@@ -0,0 +1,2 @@
+# Ej 3.1
+xmllint --xpath "APML/Body/Profile//Concepts/" ../apml1.xml
