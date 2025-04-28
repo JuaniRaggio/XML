566d92da4589d95055ef50a003f66cd6d54ff3e6
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-04-28 14:22:14 -0300

    Empiezo guia de normalizacion

--- xpath_ejercicios/tp4/ejercicio2.sh
+++ xpath_ejercicios/tp4/ejercicio2.sh
@@ -9,5 +9,7 @@ xmllint --xpath "APML/Body/Profile//Concepts/Concept[@value>=0.95]" apml1.xml
 xmllint --xpath "APML/Body/Profile/@name[count(..//Concepts/Concept) < (count(//APML/Body/Profile//Concepts/Concept) div count(//APML/Body/Profile))]" apml1.xml
 
 # Ej 2.4
-xmllint --xpath "APML/Body/Profile//Concepts/Concept/@key[starts-with(., 'a') or starts-with(., 'e') or starts-with(., 'i') or starts-with(., 'o') or starts-with(., 'u')]" apml1.xml
+xmllint --xpath \
+"APML/Body/Profile//Concepts/Concept/@key[starts-with(., 'a') or starts-with(., 'e') or starts-with(., 'i') or starts-with(., 'o') or starts-with(., 'u')]" \
+apml1.xml
 
